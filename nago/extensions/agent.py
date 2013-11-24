0935f2335a5408c890bf8c36cb5e6f82d761cc85
Pall Sigurdsson <palli@opensource.is>
2013-11-24 11:53:23 +0000

    new api extension agent

---
+++ nago/extensions/agent.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+""" Manage settings on a local node """
+
+import nago.core
+from nago.core import nago_access
+import nago.protocols.httpserver
+
+@nago_access
+def start(debug=False):
+    """ starts a nago agent (daemon) process """
+    if debug:
+        debug = True
+    nago.protocols.httpserver.app.run(debug=debug)
+
+
+@nago_access
+def stop(key, section='main'):
+    """ stops the nago agent
+    """
+    pass
