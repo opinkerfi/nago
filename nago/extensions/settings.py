1f840cdb0860d310c508a2fc329dc2fe6a8a362f
Pall Sigurdsson <palli@opensource.is>
2013-11-27 13:53:58 +0000

    pep8 compatibility in settings

--- nago/extensions/settings.py
+++ nago/extensions/settings.py
@@ -2,24 +2,24 @@
 
 """ Manage settings on a local node """
 
-import nago.core
 import ConfigParser
 from nago.core import nago_access
 
-@nago_access()
-def set(section='main', **kwargs):
+
+@nago_access(name='set')
+def edit_settings(section='main', **kwargs):
     """ Change a single option in local configuration """
     cfg_file = "/etc/nago/nago.ini"
     config = ConfigParser.ConfigParser()
     config.read(cfg_file)
     for k, v in kwargs.items():
         config.set(section=section, option=k, value=v)
-    with open(cfg_file,'w') as f:
+    with open(cfg_file, 'w') as f:
         config.write(f)
     return "Done"
 
 
-@nago_access()
+@nago_access(name='get')
 def get(key, section='main'):
     """ Get a single option from """
     cfg_file = "/etc/nago/nago.ini"
