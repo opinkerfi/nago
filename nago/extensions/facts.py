89dba0d42172160bffcffacc20667d0306480798
Pall Sigurdsson <palli@opensource.is>
2013-12-02 13:30:17 +0000

    facts api updated. anyone can now post data

--- nago/extensions/facts.py
+++ nago/extensions/facts.py
@@ -9,6 +9,9 @@ posting facts is also supported, which means the agent will store facts about a 
 
 from nago.core import nago_access
 from nago.core import get_nodes
+import nago
+import nago.extensions.settings
+import nago.extensions.info
 import json
 facts = {}
 from pynag.Utils import runCommand
@@ -25,21 +28,24 @@ def get():
     facts = json.loads(json_facts)
     return facts
 
-@nago_access()
+@nago_access(access_required="node")
 def post(host_token, **kwargs):
     """ Store facts about a remote host in memory.
 
     Facts will be stored in nago.extensions.facts with the remote token as a key
 
     """
-    facts[host_token] = kwargs
+    nago.extensions.info.node_data[host_token]['facts'] = kwargs
     return "thanks!"
 
 
 @nago_access()
 def get_all():
     """ Get all facts about all nodes """
-    return facts
+    result = {}
+    for k,v in nago.extensions.info.node_data.items():
+        result[k] = v.get('facts', {})
+    return result
 
 @nago_access()
 def send(remote_host=None):
@@ -48,19 +54,16 @@ def send(remote_host=None):
     if remote_host is provided, data will be sent to that host. Otherwise it will be sent to master.
     """
     my_facts = get()
-    remote_node = None  # Here we will store the Node we connect to
-
-    for token, i in get_nodes().items():
-        if token == remote_host:
-            remote_node = i
-            break
-        elif remote_host is None and i.get('access') == 'master':
-            remote_node = i
-            break
+    if not remote_host:
+        remote_host = nago.extensions.settings.get('server')
+    remote_node = nago.core.get_node(remote_host)
     if not remote_node:
         raise Exception("Remote host with token='%s' not found" % remote_host)
-    result = remote_node.send_command('facts', 'post', host_token=remote_node.token, **my_facts)
-    return "sent %s facts to remote node '%s'" % (len(my_facts), remote_node.token)
+    response = remote_node.send_command('facts', 'post', host_token=remote_node.token, **my_facts)
+    result = {}
+    result['server_response'] = response
+    result['message'] = "sent %s facts to remote node '%s'" % (len(my_facts), remote_node.get('host_name'))
+    return result
 
 
 
